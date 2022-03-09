efd935af4dac62b9ac1b016e5108216b459d5eec
Anderson Costa <andersscosta2@gmail.com>
2022-03-09 09:25:45 -0300

    first commit

---
+++ conhecimento.py
@@ -0,0 +1,5 @@
+numero = int(input('Digite um número inteiro qualquer: '))
+if numero %3 == 0 and numero%5 == 0:
+    print('O número é divisível por 3 e 5')
+else:
+    print('O número NÃO é divisível por 3 e 5')
